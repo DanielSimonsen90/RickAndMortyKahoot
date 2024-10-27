7ac3bc443dc4c261f2ccdaa9ae48fbd98da2add1
DanielSimonsen90 <danielsimonsen90@gmail.com>
2024-10-27 19:47:26 +0100

    User models

---
+++ RickAndMortyKahoot/Models/User/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RickAndMortyKahoot.Models.User;
+
+public class User(string username)
+{
+  public Guid? Id { get; set; }
+  public string Username { get; } = username;
+
+  public Guid? GameId { get; set; }
+}
---
+++ RickAndMortyKahoot/Models/User/UserPayload.cs
@@ -0,0 +1,6 @@
+﻿namespace RickAndMortyKahoot.Models.User;
+
+public class UserPayload(string username)
+{
+  public string Username { get; set; } = username;
+}
