4554a5d48947d76be3258b0f380baf03f094e0f4
DanielSimonsen90 <danielsimonsen90@gmail.com>
2024-10-28 01:22:14 +0100

    Create ProjectStore.cs

---
+++ RickAndMortyKahoot/Stores/ProjectStore.cs
@@ -0,0 +1,10 @@
+﻿using RickAndMortyKahoot.Models;
+using RickAndMortyKahoot.Models.Users;
+
+namespace RickAndMortyKahoot.Stores;
+
+public class ProjectStore
+{
+  public List<User> Users { get; set; } = [];
+  public List<Game> Games { get; set; } = [];
+}
